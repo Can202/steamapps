6345fbece244c52ddeb1cb3696b3513db23fe502
Manuel González <67566612+Can202@users.noreply.github.com>
2021-06-09 20:19:14 -0400

    Update su.sh

--- su.sh
+++ su.sh
@@ -3,7 +3,7 @@ echo put me in your document folder
 echo "if this folder (steamapps) is not in the document folder, it does not work"
 
 su -c "cp steam/steam /usr/bin/"
-echo Flatpak in steam command
+echo "Flatpak in steam command done.."
 
 su -c "cp -r ../../.var/app/com.valvesoftware.Steam/.local/share/icons ../../.local/share/"
 echo Icons done..
